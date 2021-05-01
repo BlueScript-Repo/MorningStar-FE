4985c95e9c11c724cfdddeadb51a28740c479644
Mayur Bhakare <uday.bajare@bluescripts.in>
2021-05-01 20:45:40 +0530

    polished upload Data

--- src/app/update/Sightseeing.ts
+++ src/app/update/Sightseeing.ts
@@ -1,8 +1,8 @@
 export class Sightseeing{
     public destinationId: string="";
-    public endTime: string="";
+    public endTime: Date=new Date();
     public name: string="";
     public rate: string="";
-    public startTime: string="";
+    public startTime: Date=new Date();
     public subDestinationId: string="";
 }
